086ce938e795fb918575dbb458f28a22392dc5ef
DouglasMiqueias <163609389+DouglasMiqueias@users.noreply.github.com>
2025-02-26 11:55:48 -0300

    Add files via upload

---
+++ AulaPOO1/src/aulapoo1/AulaPOO1.java
@@ -0,0 +1,13 @@
+package aulapoo1;
+
+import javax.swing.JOptionPane;
+
+public class AulaPOO1 
+{
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo, meu nome é Douglas!!");
+        System.out.println("POO1");
+        
+        JOptionPane.showMessageDialog(null, "Estou estudando Programacao Orientada a Objetos");
+}    
+}
