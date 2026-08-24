653f80f4d86df97626e48a113c6ff42802d9ebe6
Douglas Miqueias <163609389+DouglasMiqueias@users.noreply.github.com>
2026-08-24 14:09:52 -0300

    Add files via upload

---
+++ ConceitosBasicos-CursoADS/PrjAulaPolimorfismo/src/prjaulapolimorfismo/Moto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prjaulapolimorfismo;
+
+public class Moto extends Veiculo
+{
+    private int cilindradas;
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    public void setCilindradas(int cilindradas) {
+        this.cilindradas = cilindradas;
+    }
+    
+}
